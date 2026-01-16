7252814820f9555dc5d70ce08ca23dca5978fa7a
Hafizhcode <luthfan2107@gmail.com>
2026-01-16 20:12:23 +0700

    Jobsheet 2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SekolahController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route menggunakan Controller
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
+Route::get('/about-me', [PostController::class, 'about'])->name('posts.about');
+
+Route::get('/profil/{id}', [SekolahController::class, 'show']);
