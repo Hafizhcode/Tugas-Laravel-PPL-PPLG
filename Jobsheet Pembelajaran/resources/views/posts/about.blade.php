0d1a57ea33cc15a26b901602d4a6c9a0939595f0
Hafizhcode <luthfan2107@gmail.com>
2026-01-24 20:20:30 +0700

    Pembaruan Folder

---
+++ Jobsheet Pembelajaran/resources/views/posts/about.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>About Post</title>
+</head>
+
+<body>
+    <h1>Selamat Datang di Halaman About</h1>
+    <p>Ini adalah data yang dipanggil melalui Controller.</p>
+</body>
+
+</html>
